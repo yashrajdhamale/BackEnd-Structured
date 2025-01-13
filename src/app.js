e96964f630b9aca5905cdc6e7d7f6f8f22d5c1bc
YASHRAJ DHAMALE <yashrajdhamale15@gmail.com>
2025-01-13 17:44:01 +0530

    added cross origin for github frontend

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json()); 
 app.use(cors({
-    origin: 'http://localhost:3000', 
+    origin: 'https://yashrajdhamale.github.io', 
     credentials: true,              
 }));
 
